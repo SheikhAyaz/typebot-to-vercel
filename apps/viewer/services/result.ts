f7ef64f648d43820ead7ee577984d475d85d1db7
SheikhAyaz <35729281+SheikhAyaz@users.noreply.github.com>
2022-09-22 19:49:09 +0500

    Update result.ts

--- apps/viewer/services/result.ts
+++ apps/viewer/services/result.ts
@@ -1,12 +1,14 @@
 import { Result } from 'db'
 import { sendRequest } from 'utils'
 
+
 export const createResult = async (typebotId: string) => {
   return sendRequest<Result>({
     url: `/api/typebots/${typebotId}/results`,
     method: 'POST',
   })
 }
+
 
 export const updateResult = async (resultId: string, result: Partial<Result>) =>
   sendRequest<Result>({
